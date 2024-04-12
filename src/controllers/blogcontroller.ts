0ea085b0e218e6da52a5435638cdc9d093823f3a
GSinseswa721 <glorianiyonkurusinseswa@gmail.com>
2024-04-11 17:05:07 -0700

    comments and likes APIS

--- src/controllers/blogcontroller.ts
+++ src/controllers/blogcontroller.ts
@@ -1,6 +1,8 @@
 import express, { request } from "express";
 import { BlogModel } from "../models/blog";
 import uploadFile from "../utils/cloudinary";
+import { Request, Response } from 'express';
+import CommentModel, { IComment } from '../models/comment';
 
 class BlogController{
 
@@ -81,7 +83,130 @@ class BlogController{
             return response.sendStatus(400);
         }
     }
-}
+
+    
+      addComment = async (request: Request, response: Response): Promise<void> => {
+        try {
+          const { comment, name } = request.body;
+          const { id: blogId } = request.params;
+    
+          const blog = await BlogModel.findById(blogId);
+          if (!blog) {
+            response.status(404).json({ message: 'Blog not found' });
+            return;
+          }
+    
+          const newComment: IComment = new CommentModel({
+            name, // Ensure 'name' is included from request body
+            comment,
+            blog: blog._id,
+          });
+    
+          await newComment.save();
+    
+          // Add the new comment to the blog's comments array
+          blog.comments.push(newComment);
+          await blog.save();
+    
+          response.status(201).json({ message: 'Comment added to blog', data: newComment });
+        } catch (error) {
+          console.error(error);
+          response.status(500).json({ message: 'Internal server error' });
+        }
+      };
+    
+    
+      getCommentsForBlog = async (request: Request, response: Response): Promise<void> => {
+        try {
+          const { id: blogId } = request.params;
+    
+          const comments = await CommentModel.find({ blog: blogId });
+          response.status(200).json({ data: comments });
+        } catch (error) {
+          console.error(error);
+          response.status(500).json({ message: 'Internal server error' });
+        }
+      };
+    
+      updateComment = async (request: Request, response: Response): Promise<void> => {
+        try {
+          const { id: commentId } = request.params;
+          const { comment } = request.body;
+    
+          const updatedComment = await CommentModel.findByIdAndUpdate(
+            commentId,
+            { comment },
+            { new: true }
+          );
+    
+          if (!updatedComment) {
+            response.status(404).json({ message: 'Comment not found' });
+            return; // Ensure to return after sending the response
+          }
+    
+          response.status(200).json({ message: 'Comment updated', data: updatedComment });
+        } catch (error) {
+          console.error(error);
+          response.status(500).json({ message: 'Internal server error' });
+        }
+      };
+    
+    
+
+      deleteComment = async (request: Request, response: Response): Promise<void> => {
+        try {
+          const { id: commentId } = request.params;
+    
+          const deletedComment = await CommentModel.findByIdAndDelete(commentId);
+    
+          if (!deletedComment) {
+            response.status(404).json({ message: 'Comment not found' });
+            return; // Ensure to return after sending the response
+          }
+    
+          // Remove the comment from the associated blog's comments array
+          const blog = await BlogModel.findById(deletedComment.blog);
+          if (blog) {
+            blog.comments.pull(deletedComment._id);
+            await blog.save();
+          }
+    
+          response.status(200).json({ message: 'Comment deleted' });
+        } catch (error) {
+          console.error(error);
+          response.status(500).json({ message: 'Internal server error' });
+        }
+      };
+
+      likeBlog = async (request: Request, response: Response): Promise<void> => {
+        try {
+          const { id } = request.params;
+    
+          const blog = await BlogModel.findById(id);
+          if (!blog) {
+            response.status(404).json({ message: 'Blog not found' });
+            return;
+          }
+    
+          // Increment the likes count
+          blog.likes += 1;
+          await blog.save();
+    
+          response.status(200).json({ message: 'Like added to the blog', likes: blog.likes });
+        } catch (error) {
+          console.error(error);
+          response.status(500).json({ message: 'Internal server error' });
+        }
+      };
+
+
+    }
+    
+  
+
+// export default new BlogController();
+
+
 
 
 export default new BlogController();
